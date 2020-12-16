7251f98d10deaec0f19c21a1a4abbf4edeb30092
Nicklas Jensen <nicklas.jensen@pexip.com>
2020-12-16 15:12:16 +0100

    Do not go below 0 in quantity

--- fetchAndUpdateProducts.js
+++ fetchAndUpdateProducts.js
@@ -235,7 +235,7 @@ async function updateItems() {
       if (foundItem) {
         itemUpdates.push({
           name: fItem.name,
-          quantity: foundItem.in_stock,
+          quantity: foundItem.in_stock <= 0 ? 0 : foundItem.in_stock,
           variantId: fItem.sVariantId,
           prevQuantity: fItem.sQuantity,
           imageUrl: fItem.imageUrl,
